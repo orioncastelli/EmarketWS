07166133a43252b35e59259dee79f395ad619f24
orion <orion@ORCS-FULLRANGE>
2016-11-19 18:26:35 -0200

    Add project files.

---
+++ Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmarketWS
+{
+    public class Category
+    {
+            public string idCategory
+        {
+            get;
+            set;
+        }
+
+        public string idStore
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
